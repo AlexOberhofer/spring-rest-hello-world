dcc3471340dad2a94f95b86fb1986e7f15b66319
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-05-28 20:17:01 -0400

    ddl update

--- db.sql
+++ db.sql
@@ -1,8 +1,10 @@
 CREATE DATABASE IF NOT EXISTS sample;
 
+CREATE SCHEMA IF NOT EXISTS SAMPLE;
+
 CREATE TABLE IF NOT EXISTS sample.SampleTable (
     requestId int NOT NULL AUTO_INCREMENT,
     name varchar(50),
     requestTime timestamp,
     PRIMARY KEY (requestId)
-    );
+);
